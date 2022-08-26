d35cd28e69df363726d85d9fb16c981f2464604a
Basudev <murabasudev@gmail.com>
2022-08-26 10:47:02 +0530

    fixed Typo error PORT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve("client", "index.html"));
 });
 
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
 });
