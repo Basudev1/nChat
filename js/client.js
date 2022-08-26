af0bafac47d724cfabfec3fde2f98936bda23afb
Basudev <murabasudev@gmail.com>
2022-08-26 07:49:25 +0530

    changed port

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:4000');
+const socket = io('https://nchat-production.up.railway.app:80');
 const form = document.getElementById('send-container');
 const msgInput = document.getElementById('msgInp');
 const msgContainer = document.querySelector('.container');
